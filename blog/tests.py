38ba7c95f14833f279e5f0c0cc7b35ad23900e16
SergeyV <mamayotto@gmail.com>
2020-11-23 08:15:29 -0500

    Added blog/templates/*.html, blog/static/*css folders and files.

---
+++ blog/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+'''
+############################################
+# The code snipet is from blog/views.py
+# this is here only for pedagogical purpose.
+# 
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+'''
---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+posts = [ { 
+			'author': 'Sergey Vardanyan',
+			'title': 'Blog Post 1',
+			'content': 'First Blog content',
+			'date_published': 'November 22, 2020'
+			},
+		{
+			'author': 'Corey Shafer',
+			'title': 'Blog Post 2',
+			'content': 'Second Blog content',
+			'date_published': 'November 23, 2020'
+			}
+		]
+
+def home(request):
+	context = {'posts': posts}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	context = {'title': 'About'}
+	return render(request, 'blog/about.html', context)
+
