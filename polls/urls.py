394217b7b9684aed338e9e109a9181401cf3f9aa
Sergey Vardanyan <mamayotto@gmail.com>
2020-12-24 18:57:32 -0500

    Add generic TemplateView to polls.

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('polls/', views.IndexView.as_view())
+    path('polls/',TemplateView.as_view(template_name='polls/main.html')),
+    path('polls/index/', views.IndexView.as_view()),
 ]
