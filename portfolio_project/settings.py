39ae3a8d9d633024c2128d2e1e845fa528a37aa6
Sergey Vardanyan <mamayotto@gmail.com>
2020-12-25 20:39:39 -0500

    Create initial settings for airtable.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from os import environ
+load_dotenv()
 
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
