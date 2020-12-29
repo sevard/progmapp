c60eb06503fa6a57a0bf8774ddb79e7cdc11f05b
Sergey Vardanyan <mamayotto@gmail.com>
2020-12-29 16:30:54 -0500

    Modify apps url.py files to be able to interlink the apps.

--- games/urls.py
+++ games/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.views.generic import TemplateView
 
 
+app_name = 'games'
 urlpatterns = [
-    path('games/', TemplateView.as_view(template_name='games/main.html')),
+    path('games/', TemplateView.as_view(template_name='games/main.html'), name='games-main'),
     path('games/<slug:guess>', views.GameView.as_view())
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.views.generic import TemplateView
 
 
+app_name = 'polls'
 urlpatterns = [
-    path('polls/',TemplateView.as_view(template_name='polls/main.html')),
+    path('polls/',TemplateView.as_view(template_name='polls/main.html'), name='polls-main'),
     path('polls/index/', views.IndexView.as_view()),
 ]
