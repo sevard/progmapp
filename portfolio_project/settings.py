7c9531f813821151e835e053049a9900153bc398
Sergey Vardanyan <mamayotto@gmail.com>
2021-01-10 17:15:40 -0500

    Create common base.html for all the apps. Fix templates accordingly.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 
@@ -62,7 +61,7 @@ ROOT_URLCONF = 'portfolio_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
