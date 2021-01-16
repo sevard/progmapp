ed1bc3023ed51d6f6a97619fa606fb48ff9c69c1
Sergey Vardanyan <mamayotto@gmail.com>
2021-01-16 16:38:11 -0500

    Remove custom css files.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -61,8 +61,7 @@ ROOT_URLCONF = 'portfolio_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 os.path.join(BASE_DIR, 'static')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
