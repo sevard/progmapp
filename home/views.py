62c6b1ebe9f0742c3cab1e19f86c628899de647c
Sergey Vardanyan <srg.v@posteo.us>
2023-08-06 09:44:41 -0400

    Update home views.py

--- home/views.py
+++ home/views.py
@@ -2,5 +2,9 @@ from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home/base.html')
+
+
 def index(request):
     return render(request, 'home/index.html')
