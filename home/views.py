3c9aa48341f1bb34484140cf3d34e08c96b46985
Sergey Vardanyan <srg.v@posteo.us>
2023-08-06 11:03:04 -0400

    Format views.py

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
-from unicodedata import name
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'home/base.html')
+    return render(request, 'home/home.html')
 
 
 def index(request):
