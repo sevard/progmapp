d96671cd51f99394f447f97eb412f370faaf88b9
Sergey Vardanyan <srg.v@posteo.us>
2023-08-06 15:05:42 -0400

    Remove about page from blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,4 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 
 
-posts = [ {
-			'author': 'John Doe',
-			'title': 'Blog Post 1',
-			'content': 'First Blog content',
-			'date_published': 'November 22, 2020'
-			},
-		{
-			'author': 'Bob Smith',
-			'title': 'Blog Post 2',
-			'content': 'Second Blog content',
-			'date_published': 'November 23, 2020'
-			}
-		]
+posts = [{
+    'author': 'John Doe',
+    'title': 'Blog Post 1',
+    'content': 'First Blog content',
+    'date_published': 'November 22, 2020'
+},
+    {
+    'author': 'Bob Smith',
+    'title': 'Blog Post 2',
+    'content': 'Second Blog content',
+    'date_published': 'November 23, 2020'
+}
+]
+
 
 def home(request):
-	context = {'posts': posts}
-	return render(request, 'blog/home.html', context)
-
-def about(request):
-	context = {'title': 'About'}
-	return render(request, 'blog/about.html', context)
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
