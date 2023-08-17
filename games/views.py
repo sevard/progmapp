7c84674baf0f20a5bb4f964c1b0932de1df1dfa5
Sergey Vardanyan <srg.v@posteo.us>
2023-08-16 22:05:10 -0400

    Update guess game to organize it a bit better

--- games/views.py
+++ games/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render
 from django.views import View
 
 
-class GameView(View):
+class GamesView(View):
     """docstring for GameView."""
 
+    def get(self, request):
+        return render(request, 'games/index.html')
+
+
+class GuessView(View):
+    """Description here"""
+
+    def get(self, request):
+        return render(request, 'games/guess.html')
+
+
+class GuessPlayView(View):
+    """docstring for GuessView."""
+
     def get(self, request, guess):
+
         try:
             x = {'guess': int(guess)}
         except:
