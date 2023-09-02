f0b1c625017baec092787eea1b8566e58789852b
Sergey Vardanyan <srg.v@posteo.us>
2023-09-02 10:46:50 -0400

    Remove ustil app

--- utils/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'utils/index.html')
-
-
-def sha_functions(request):
-    return render(request, 'utils/sha-functions.html')
