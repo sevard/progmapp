45f379d5217a4a79ffc2af49ec0950f87f24ee14
sergey vardanyan <srg.v@posteo.us>
2024-12-20 02:14:56 +0000

    Set django debug setting

--- progmapp/settings.py
+++ progmapp/settings.py
@@ -27,8 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('DJANGO_ENV') == "dev" else False
-# print("DEBUG=", type(DEBUG), DEBUG)
+DEBUG = False 
 
 ALLOWED_HOSTS = ['progmapp.com', '127.0.0.1']
 
